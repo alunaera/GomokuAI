e95dd3db074892095c42aeaa3a0b54849c1d966f
eo39 <zhekaonichsenko@gmail.com>
2020-01-31 16:39:16 +0200

    v0.1a
    Видит открытые четверки по горизонтали и вертикали

---
+++ GomokuAI/GomokuTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace GomokuAI
+{
+    [TestFixture]
+    internal class GomokuTests
+    {
+        [Test]
+        public void MyFirstTest()
+        {
+            int x = 2;
+            int y = 2;
+            int result = x * y;
+            
+            Assert.That(result, Is.EqualTo(4));
+        }
+    }
+}
--- GomokuAI/Program.cs
+++ GomokuAI/Program.cs
@@ -10,8 +10,8 @@ namespace GomokuAI
 
             while (true)
             {
-                int x = 2;
-                int y = 1;
+                int x = -1;
+                int y = -1;
                 int player, n;
 
                 int[,] map = new int[100, 100];
@@ -20,21 +20,56 @@ namespace GomokuAI
                 n = int.Parse(Console.ReadLine());
 
                 for (int i = 1; i <= n; ++i)
+                {
+                    int comboPointCount = 0;
                     for (int j = 1; j <= n; ++j)
                     {
                         map[i, j] = int.Parse(Console.ReadLine());
-                    }
 
-                while (map[x,y] != 0)
-                {
-                    x = random.Next(1, 15);
-                    y = random.Next(1, 15);
+                        if (comboPointCount == 4 && j < 15 && map[i, j] == 0)
+                        {
+                            x = i;
+                            y = j;
+                        }
+
+                        if (map[i, j] == player)
+                            comboPointCount++;
+                        else
+                            comboPointCount = 0;
+                    }
                 }
 
-                Console.WriteLine("{0}:{1}",x,y);
+                for (int j = 1; j <= n; j++)
+                {
+                    int comboPointCount = 0;
+                    for (int i = 1; i <= n; i++)
+                    {
+                        if (comboPointCount == 4 && i < 15 && map[i, j] == 0)
+                        {
+                            x = i;
+                            y = j;
+                        }
+
+                        if (map[i, j] == player)
+                            comboPointCount++;
+                        else
+                            comboPointCount = 0;
+                    }
+                }
+
+                if (x == -1 || y == -1)
+                {
+                    do
+                    {
+                        x = random.Next(4, 11);
+                        y = random.Next(4, 11);
+                    } 
+                    while (map[x, y] != 0);
+                }
+
+
+                Console.WriteLine("{0}:{1}", x, y);
             }
         }
-
-
     }
 }

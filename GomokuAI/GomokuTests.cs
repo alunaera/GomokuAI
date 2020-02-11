dda99e03d6ae47b153a8d1c14978687a3c1a97ce
eo39 <zhekaonichsenko@gmail.com>
2020-02-11 14:50:33 +0200

    Update GomokuTests.cs

--- GomokuAI/GomokuTests.cs
+++ GomokuAI/GomokuTests.cs
@@ -9,16 +9,16 @@ namespace GomokuAI
     {
         [TestCase("1 1", "1 2", "1 3", "1 4", ExpectedResult = "1 5")]
         [TestCase("1 1", "2 1", "3 1", "4 1", ExpectedResult = "5 1")]
-        [TestCase("12 1", "13 1", "14 1", "15 1", ExpectedResult = "11 1")]
-        [TestCase("12 11", "13 12", "14 13", "15 14", ExpectedResult = "11 10")]
-        [TestCase("2 1", "3 2", "4 3", "5 4", ExpectedResult = "6 5")]
-        [TestCase("14 1", "13 2", "12 3", "11 4", ExpectedResult = "10 5")]
-        [TestCase("13 12", "12 13", "11 14", "10 15", ExpectedResult = "14 11")]
         [TestCase("1 1", "1 2", "1 4", "1 5", ExpectedResult = "1 3")]
         [TestCase("1 1", "2 1", "4 1", "5 1", ExpectedResult = "3 1")]
         [TestCase("1 1", "2 2", "3 3", "4 4", ExpectedResult = "5 5")]
+        [TestCase("2 1", "3 2", "4 3", "5 4", ExpectedResult = "6 5")]
+        [TestCase("12 1", "13 1", "14 1", "15 1", ExpectedResult = "11 1")]
+        [TestCase("14 1", "13 2", "12 3", "11 4", ExpectedResult = "10 5")]
         [TestCase("15 1", "14 2", "12 4", "11 5", ExpectedResult = "13 3")]
-
+        [TestCase("13 12", "12 13", "11 14", "10 15", ExpectedResult = "14 11")]
+        [TestCase("12 11", "13 12", "14 13", "15 14", ExpectedResult = "11 10")]
+        
         public string TestGetNextWinningTurn(params string[] points)
         {
             const int playerNumber = 1;

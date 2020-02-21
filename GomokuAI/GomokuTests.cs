36f4ebc312e19894c198610e3ecfcea59ca3d1db
eo39 <zhekaonichsenko@gmail.com>
2020-02-21 12:55:36 +0200

    v1.0a
    В тестах добавлена проверка на производительность

--- GomokuAI/GomokuTests.cs
+++ GomokuAI/GomokuTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System.Diagnostics;
+using System.Drawing;
 using NUnit.Framework;
 using static GomokuAI.Program;
 
@@ -33,7 +34,13 @@ namespace GomokuAI
                 gameField[int.Parse(s[0]), int.Parse(s[1])] = playerNumber;
             }
 
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
             Point? nextTurn = GetBallNextPosition(gameField, gameFieldSize, playerNumber);
+
+            stopwatch.Stop();
+
+            Assert.IsTrue(stopwatch.Elapsed.Milliseconds < 20);
 
             return nextTurn.HasValue
                 ? $"{nextTurn.Value.X} {nextTurn.Value.Y}"
